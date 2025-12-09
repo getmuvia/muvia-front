7d26da6034f0237a01a0576438a1f5bce4b1c4db
jherson <rodriguezpaul98@gmail.com>
2025-12-09 17:01:39 -0400

    feat(ui): create shop-navbar component

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ShopNavbar } from './layout/components/shop-navbar/shop-navbar';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ShopNavbar],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
