e9ec1f075161cf9fe4c211048c030b971af50907
jherson <rodriguezpaul98@gmail.com>
2025-12-09 21:28:16 -0400

    feat(routing): configure nested routes for shop and seller scopes

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,6 +2,10 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: 'seller',
+    renderMode: RenderMode.Prerender
+  },
+  {
     path: '**',
     renderMode: RenderMode.Prerender
   }
