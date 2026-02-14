05ffceac24d7db955797c4af000d9eee73897e4f
jherson <rodriguezpaul98@gmail.com>
2026-02-14 00:51:38 -0400

    fix routes server

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -10,6 +10,10 @@ export const serverRoutes: ServerRoute[] = [
     renderMode: RenderMode.Server
   },
   {
+    path: 'seller/products/:id/edit',
+    renderMode: RenderMode.Server
+  },
+  {
     path: '**',
     renderMode: RenderMode.Prerender
   }
